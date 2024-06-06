606026147dac3e71e8b120dda6c20dc6531fcdaa
Shreyas-Mane <shreyas12mane@gmail.com>
2024-06-06 12:57:48 +0530

    changes9

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,5 @@
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
-
 import users from '../models/auth.js'
 
 export const signup = async(req,res)=>{
@@ -32,7 +31,7 @@ export const login = async(req,res)=>{
            if(!isPasswordCrt){
                return res.status(400).json({message:"Invalid credentials"})
            }
-           const token = jwt.sign({email:existinguser.email,id:existinguser._id},"test",{expiresIn:'1h'});
+           const token = jwt.sign({email:existinguser.email,id:existinguser._id},process.env.JWT_SECRET,{expiresIn:'1h'});
            res.status(200).json({result:existinguser,token})
     }catch(error){
         res.status(500).json("Something went wrong...")
